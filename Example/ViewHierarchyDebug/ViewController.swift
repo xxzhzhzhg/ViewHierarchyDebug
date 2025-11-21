94b7fc94e220ed0e3aa758fd212e92eca9209f98
zh.g <xxzhzhzh@gmail.com>
2025-11-21 10:30:41 +0800

    1
    1

---
+++ Example/ViewHierarchyDebug/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  ViewHierarchyDebug_Example
+//
+//  Created by xxsdf on 2025/11/21.
+//  Copyright © 2025 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
